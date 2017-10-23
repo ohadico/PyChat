adf42e6f74c69d7552385459fcbb54b7ba0121e3
Ohad Cohen <ohadc3@gmail.com>
2017-10-23 13:18:03 +0300

    Initial commit Basic chat

---
+++ client.py
@@ -0,0 +1,132 @@
+import socket
+
+SERVER_IP = '127.0.0.1'
+SERVER_PORT = 12345
+SUCCESS = "SUCCESS"
+EMPTY = "EMPTY"
+
+
+class Client(object):
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def sendrecv(self, msg):
+        self.sock.send(msg)
+        return self.sock.recv(4096)
+
+    def connect(self, ip, port):
+        self.sock.connect((ip, port))
+
+    def try_name(self, name):
+        return self.sendrecv(name)
+
+    def get_clients(self):
+        return self.sendrecv("get clients").split('|')
+
+    def send_pm(self, client_name, msg):
+        self.sock.send("send to {name} {msg}".format(name=client_name, msg=msg))
+
+    def get_messages(self):
+        """
+        :return: dict of list of messages from each client
+        """
+        messages = {}
+        response = self.sendrecv("get messages")
+        if response == EMPTY:
+            return messages
+        for msg in response.split('|'):
+            sender = msg[:msg.find(" ")]
+            content = msg[len(sender) + 1:]
+            messages.setdefault(sender, []).append(content)  # this line create list of messages from each client
+        return messages
+
+    def disconnect(self):
+        self.sock.send("bye")
+        self.sock.shutdown(socket.SHUT_RDWR)  # releases resources
+        self.sock.close()
+
+
+class ClientUI(object):
+    """
+    Handle the use of Client
+    """
+    def __init__(self, client):
+        """
+        :param client: Client
+        """
+        self.client = client
+
+    @staticmethod
+    def print_welcome_message():
+        print "Welcome to Axon Chat Room!"
+
+    def connect_to_server(self):
+        self.client.connect(SERVER_IP, SERVER_PORT)
+        name = raw_input("Please Enter your name: ")
+        server_response = self.client.try_name(name)
+        while server_response != SUCCESS:
+            print "Error:", server_response
+            name = raw_input("Please Enter another name: ")
+            server_response = self.client.try_name(name)
+        print "Hello", name
+
+    @staticmethod
+    def print_menu():
+        print "[S]end private message"
+        print "[R]ead messages"
+        print "E[x]it"
+
+    def send_pm(self):
+        # Print available clients
+        clients = self.client.get_clients()
+        for index, client_name in enumerate(clients):
+            print "[{i}] {name}".format(i=index, name=client_name)
+        index = raw_input("Choose client number: ")
+
+        # Check client index is valid
+        if not index.isdigit() or int(index) >= len(clients):
+            return
+
+        # Read and send the message
+        msg = raw_input("Enter message: ")
+        self.client.send_pm(clients[int(index)], msg)
+
+    def print_messages(self):
+        """
+        Get all messages sent to this client from server and print them
+        """
+        messages = self.client.get_messages()
+        print "You have {} messages".format(len(messages))
+        for sender, msgs in messages.items():
+            for msg in msgs:
+                print "{sender}: {msg}".format(sender=sender, msg=msg.replace("\n", "\n    "))
+
+    def exit(self):
+        """
+        Disconnect gracefully
+        """
+        self.client.disconnect()
+
+    def main_loop(self):
+        self.print_menu()
+        cmd = raw_input("Enter your command: ")
+        while cmd.lower() != 'x':
+            if cmd.lower() == 's':
+                self.send_pm()
+            elif cmd.lower() == 'r':
+                self.print_messages()
+            self.print_menu()
+            cmd = raw_input("Enter your command: ")
+        self.exit()
+
+
+def main():
+    client = Client()
+    handler = ClientUI(client)
+    handler.print_welcome_message()
+    handler.connect_to_server()
+    handler.main_loop()
+
+
+if __name__ == '__main__':
+    main()
---
+++ server.py
@@ -0,0 +1,157 @@
+import socket
+import threading
+
+PORT = 12345
+SUCCESS = "SUCCESS"
+EMPTY = "EMPTY"
+
+
+class ServerException(Exception):
+    pass
+
+
+class Server(object):
+    def __init__(self):
+        self.clients = {}
+        self.messages = {}
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def get_clients(self):
+        """
+        :return: list of clients names
+        """
+        return self.clients.keys()
+
+    def add_client(self, name, sock):
+        # Warning: not thread safe
+        if '|' in name or ' ' in name:
+            raise ServerException("Name cannot contains '|' or spaces")
+        if name in self.clients:
+            raise ServerException("This name is taken")
+        self.clients[name] = sock
+
+    def remove_client(self, name):
+        # Warning: not thread safe
+        self.clients.pop(name)
+
+    def get_messages(self, name):
+        return self.messages.get(name)
+
+    def add_message(self, source, dest, msg):
+        if dest not in self.messages:
+            self.messages[dest] = {}
+        if source not in self.messages[dest]:
+            self.messages[dest][source] = []
+        self.messages[dest][source].append(msg)
+
+    def start(self, port):
+        self.sock.bind(('0.0.0.0', port))
+        self.sock.listen(5)  # Max connections
+        print "Server is up and listening on port {port}".format(port=port)
+
+        try:
+            print "Press Ctrl+C to stop listening for new connections"
+            # listen for new connection loop
+            while True:
+                client_sock, client_address = self.sock.accept()
+                print 'New connection from {ip}:{port}'.format(ip=client_address[0], port=client_address[1])
+                ClientHandler(client_sock, client_address[0], client_address[1], self).start()
+        except KeyboardInterrupt:
+            print "Server Stopped listening for new connections"
+            print "There is still {} existing connections".format(len(self.clients))
+
+
+class ClientHandler(threading.Thread):
+    def __init__(self, sock, ip, port, server):
+        """
+        A thread to handle 1 client
+        :param sock: socket
+        :param ip: string
+        :param port: int
+        :param server: Server
+        """
+        threading.Thread.__init__(self)
+        self.sock = sock
+        self.ip = ip
+        self.port = port
+        self.server = server
+        self.client_name = None
+
+    def run(self):
+        try:
+            self.client_name = self.pick_name()
+            self.main_loop()
+        except socket.error as e:
+            print "Socket Error: {error}".format(error=e)
+        self.cleanup()
+
+    def main_loop(self):
+        cmd = self.recv_cmd()
+        while cmd != 'bye':
+            if cmd.startswith("get"):
+                if cmd == "get clients":
+                    res = self.get_clients()
+                elif cmd == "get messages":
+                    res = self.get_messages()
+                print "{name} request {what}".format(name=self.client_name, what=cmd[4:])
+                self.sock.send(res)
+            elif cmd.startswith("send to "):
+                self.send_pm(cmd)
+            cmd = self.recv_cmd()
+
+    def recv_cmd(self):
+        cmd = self.sock.recv(4096)
+        print '{name} ask: "{cmd}"'.format(name=self.client_name, cmd=cmd)
+        return cmd
+
+    def pick_name(self):
+        """
+        Try get a valid name
+        :return: name
+        """
+        while True:
+            name = self.sock.recv(4096)
+            print "{ip}:{port} trying using name: {name}".format(ip=self.ip, port=self.port, name=name)
+            try:
+                self.server.add_client(name, self.sock)
+                self.sock.send(SUCCESS)
+                print "{ip}:{port} got name: {name}".format(ip=self.ip, port=self.port, name=name)
+                return name
+            except ServerException as e:
+                self.sock.send(e.message)
+
+    def get_clients(self):
+        return '|'.join(self.server.get_clients())
+
+    def get_messages(self):
+        messages = self.server.get_messages(self.client_name)
+        if not messages:  # None or with length 0
+            return EMPTY
+        messages = ["{sender} {msg}".format(sender=sender, msg=msg) for sender, msgs in messages.items() for msg in msgs]
+        return '|'.join(messages)
+
+    def send_pm(self, request):
+        name_start = len("send to ")
+        name_end = request.find(" ", name_start+1)
+
+        to = request[name_start:name_end]
+        msg = request[name_end+1:]
+
+        print "{} -> {}: {}".format(self.client_name, to, msg)
+        self.server.add_message(self.client_name, to, msg)
+
+    def cleanup(self):
+        if self.client_name is not None:
+            self.server.remove_client(self.client_name)
+            print "{name} has left".format(name=self.client_name)
+        else:
+            print "{ip}:{port} has left".format(ip=self.ip, port=self.port)
+        self.sock.close()
+
+
+def main():
+    server = Server()
+    server.start(PORT)
+
+if __name__ == '__main__':
+    main()
